eccf2c1c272ed63cebf8df530996a32316590c9c
Sonwa Dimitry <154930240+Manos2605@users.noreply.github.com>
2024-10-13 20:58:52 +0100

    Add files via upload

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 
-int fibonacci(int n){
-    if (n == 0 || n == 1) return 1;
+int fibonacci(int n){ /* f(n) = f(n-1) + f(n-2) */
+    if (n == 0) return 0;
+    if (n == 1) return 1;
 
     int a = 0, b = 1, fib = 0;
     for (int i = 0; i < n ; i++)
